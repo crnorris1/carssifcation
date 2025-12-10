6b7c8e541aabbcb819dd3d0bcdec3fda02c99b71
crnorris1 <crnorris@wpi.edu>
2025-12-10 15:54:51 -0500

    Simple file upload, does

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+
+  const [image, setImage] = React.useState(null)
+
+  const onFileChange = (event) => {
+    setImage(event.target.files[0])
+  }
+
+  function submitImage(){
+    const formData = new FormData()
+    formData.append(
+      "image",
+      image,
+      image.name
+    )
+    alert(image.name)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Car Classifier</h1>
+      <h3>CS4342 Final Project - Cam Norris, Akaash Walker</h3>
+      <h2>Upload a picture of your car <b>from the side </b> and find out what type of car it is</h2>
+      <h2>(SUV, Sudan, Coupe, Truck)</h2>
+      <br/><br/>
+      <form>
+        <h2>Upload your photo:</h2>
+        <input type="file" id="imageUpload" onChange ={onFileChange}></input>
+        <button onClick={submitImage}>Upload</button>
+      </form>
     </div>
   );
 }
