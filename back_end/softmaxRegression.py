2476d77217f1f915877f118ffa3e9e998f3861d6
crnorris1 <crnorris@wpi.edu>
2025-12-10 16:12:44 -0500

    Python back_end. Run the python script in back_end

---
+++ back_end/softmaxRegression.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"status" : "Server is running on 5000"})
+
+@app.route("/test", methods=["GET"])
+def test():
+    name = request.args.get("name", "World")
+    return jsonify({"message": f"Hello, {name}"})
+
+def not_found(e):
+    return jsonify({"error" : "Not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+    print("hello")
