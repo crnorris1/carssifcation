012d1d6a83355db377fd91becea513cc42812ccc
crnorris1 <crnorris@wpi.edu>
2025-12-10 16:13:40 -0500

    Comments

--- back_end/softmaxRegression.py
+++ back_end/softmaxRegression.py
@@ -4,10 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+#localhost/5000
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"status" : "Server is running on 5000"})
 
+#localhost/5000/test
 @app.route("/test", methods=["GET"])
 def test():
     name = request.args.get("name", "World")
@@ -15,7 +17,6 @@ def test():
 
 def not_found(e):
     return jsonify({"error" : "Not found"}), 404
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
