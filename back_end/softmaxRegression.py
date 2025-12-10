c54b7fc39c81ea4b7402384ff23148bf818ace95
crnorris1 <crnorris@wpi.edu>
2025-12-10 16:14:54 -0500

    Commit

--- back_end/softmaxRegression.py
+++ back_end/softmaxRegression.py
@@ -10,10 +10,13 @@ def home():
     return jsonify({"status" : "Server is running on 5000"})
 
 #localhost/5000/test
+#or
+#localhost/5000/test?name=Earth outputs Hello, Earth
 @app.route("/test", methods=["GET"])
 def test():
     name = request.args.get("name", "World")
     return jsonify({"message": f"Hello, {name}"})
+
 
 def not_found(e):
     return jsonify({"error" : "Not found"}), 404
